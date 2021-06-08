848816881c1311297d9ab84817dfcbaa536e5ae7
Fikret <fburgazli@hotmail.com>
2021-06-08 10:28:16 -0400

    needed to add some changes on vy truckutils. and tried to create a new login test, because the previous one was lost :(

--- src/test/java/com/vytrack/utilities/VyTrackUtils.java
+++ src/test/java/com/vytrack/utilities/VyTrackUtils.java
@@ -10,19 +10,21 @@ import java.util.concurrent.TimeUnit;
 
 public class VyTrackUtils {
     WebDriver driver;
+
     public static void setupForVyTrack(WebDriver driver) {
 
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        driver.get("https://qa3.vytrack.com/user/login");
+       // driver.get("https://qa3.vytrack.com/user/login");
+        loginToVyTrack(driver);
     }
 
         public static void loginToVyTrack(WebDriver driver) {
 
             //driver.get(ConfigurationReader.getProperty("env"));
-            //driver.get("https://qa3.vytrack.com/user/login");
+            driver.get("https://qa3.vytrack.com/user/login");
             //BrowserUtils.sleep(10);
             driver.findElement(By.name("_username")).sendKeys(ConfigurationReader.getProperty("username"));
             driver.findElement(By.name("_password")).sendKeys(ConfigurationReader.getProperty("password") + Keys.ENTER);
