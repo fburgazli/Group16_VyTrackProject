66ccc2be5ef5f3f965a419f6f380d4c94382894a
AbigailVazquez <80226640+arteaby31@users.noreply.github.com>
2021-06-25 21:42:40 -0400

    create  LibraryLoginPageVytrack and buttons to use in the automation of the aceptance criteria

---
+++ src/test/java/com/vytrack/Page/LibraryLoginPageVytrack.java
@@ -0,0 +1,31 @@
+package com.vytrack.Page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.vytrack.utilities.Driver;
+public class LibraryLoginPageVytrack {
+
+public LibraryLoginPageVytrack(){
+
+    PageFactory.initElements(Driver.getDriver(), this); }
+    @FindBy(id = "prependedInput")
+    public WebElement inputUserName;
+    @FindBy(id="prependedInput2")
+    public WebElement inputPassword;
+    @FindBy(id="_submit")
+    public WebElement loginBtn;
+    @FindBy(xpath="//span[@class='title title-level-1'][1]")
+    public WebElement fleetBtn;
+    @FindBy(xpath = "//div[@id='container']")
+    public WebElement vehiclesBtn;
+    @FindBy(xpath = "//a[@title='With this button you will export the content of the grid as it appears to you, with filters and sorting applied. All pages will be exported.']")
+    public WebElement exportGridBtn;
+    @FindBy(xpath = "//a[@class='action btn refresh-action mode-text-only']")
+    public WebElement refreshBtn;
+    @FindBy(xpath = "//a[@class='action btn reset-action mode-text-only']")
+    public WebElement resetBtn;
+
+
+}
---
+++ src/test/java/com/vytrack/tests/AC1ClickExportGridDropdown.java
@@ -0,0 +1,21 @@
+package com.vytrack.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AC1ClickExportGridDropdown extends TestBase{
+/**
+ Step 1- I  Go to VyTrack login page https://qa3.vytrack.com/user/login
+ Step 2- I Login as an Truck Driver, Enter correct user name
+ Step 3- I Enter correct password
+ Step 4- I Click login button
+ Step 5- Fleet Management page should show in the screen
+ Step 6- I Click on the Export Grid dropdown button
+ Step 7- I should see the ExportGrid button dropdown shows two options
+ TEST PASS
+ *
+ */
+
+
+
+}
